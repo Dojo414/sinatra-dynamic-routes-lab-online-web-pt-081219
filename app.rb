bc3112135977c95c4d973ae36048db2aeea9ab01
Donovan Jones <donovanljones94@gmail.com>
2019-11-20 01:37:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,6 +37,11 @@ class App < Sinatra::Base
     case @operation
     when "subtract"
       (@number1 - @number2).to_s
-      
+    when "add"
+      (@number1 + @number2).to_s
+    when "multiply"
+      (@number1 * @number2).to_s
+    when "divide"
+      (@number1 / @number2).to_s
 
 end
